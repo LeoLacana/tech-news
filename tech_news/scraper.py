008ed60a591eee5f1301a013d96cf0f16e5d7776
LeoLacana <wilson17414@gmail.com>
2022-02-18 18:41:28 -0300

    req 4 done

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -41,8 +41,84 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
+def scrape_write(selector):
+    try:
+        getWriter = (
+            selector.css("a.tec--author__info__link::text").get()
+        ).strip()
+    except AttributeError:
+        try:
+            getWriter = (
+                selector.css("div.tec--timestamp__item > a::text").get()
+            ).strip()
+        except AttributeError:
+            getWriter = (
+                selector.css("div.tec--author__info p:first-child::text").get()
+            )
+    return getWriter
+
+
+def scrape_shares_count(selector):
+    try:
+        getSharesCount = int(
+            selector.css(
+                "nav.tec--toolbar div.tec--toolbar__item::text"
+            ).re_first(r"(\d*)(?:\s*Compartilharam*$)")
+        )
+    except TypeError:
+        getSharesCount = 0
+    return getSharesCount
+
+
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+
+    getUrl = selector.css("link[rel='canonical']::attr(href)").get()
+
+    getTitle = selector.css("h1.tec--article__header__title::text").get()
+
+    getTimestamp = selector.css(
+        ".tec--timestamp__item time#js-article-date::attr(datetime)").get()
+
+    getWriter = scrape_write(selector)
+
+    getSharesCount = scrape_shares_count(selector)
+
+    getCommentsCount = selector.css(
+            "nav.tec--toolbar button#js-comments-btn::attr(data-count)"
+        ).get()
+    if getCommentsCount is None:
+        getCommentsCount = 0
+
+    getSummary = "".join(
+        selector.css("div.tec--article__body p:first-child *::text").getall()
+    )
+
+    getSourcer = selector.css(
+        "div.z--mb-16 a::text").getall()
+    getSourcerRefatored = []
+    for source in getSourcer:
+        getSourcerRefatored.append(source.strip())
+
+    getCategories = selector.css(
+        "#js-categories a::text").getall()
+    getCategoriesRefatored = []
+    for category in getCategories:
+        getCategoriesRefatored.append(category.strip())
+
+    response = {
+        "url": getUrl,
+        "title": getTitle,
+        "timestamp": getTimestamp,
+        "writer": getWriter,
+        "shares_count": getSharesCount,
+        "comments_count": int(getCommentsCount),
+        "summary": getSummary,
+        "sources": getSourcerRefatored,
+        "categories": getCategoriesRefatored,
+    }
+    return response
 
 
 # Requisito 5
