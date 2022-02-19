482da2eb7f11ae842e5ae5854f78b4f209279be0
LeoLacana <wilson17414@gmail.com>
2022-02-19 03:57:55 -0300

    req 7 done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,32 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    response = [
+        (news["title"], news["url"])
+        for news in search_news(
+            {"title": {"$regex": f"{title}", "$options": "i"}})
+    ]
+    return response
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.fromisoformat(date)
+        response = [
+            (news['title'], news['url'])
+            for news in search_news(
+                {"timestamp": {"$regex": f"{date}"}}
+            )
+        ]
+    except ValueError:
+        raise ValueError("Data inválida")
+    return response
 
 
 # Requisito 8
